cffe2f57e6f56357686248cc60d8d642e7534683
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-25 19:45:31 +0100

    restricted request url to /

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ server.listen(port, hostname, () => {
 
 
 function homeRoute(request, response) {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
-    
+    if(request.url === "/") {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.write("Header\n");
+        response.write("Search\n");
+        response.end("Footer\n");
+    }
+
 }
