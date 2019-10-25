d8489337da5095aa6b4748fb9ffa10130ea0a95a
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-25 19:19:26 +0100

    added some responses to the web server

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = 3000;
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
+    response.write("This is before the end \n");
     response.end('Hello World\n');
+    response.write("This is after the end");
 });
 
 server.listen(port, hostname, () => {
