35edae62e1c9c6b6e0a91687ec2c9de1e7c398da
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-25 19:23:39 +0100

    printing date and time to the website every second

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const port = 3000;
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.write("This is before the end \n");
-    response.end('Hello World\n');
-    response.write("This is after the end");
+    setInterval(function(){
+        response.write(new Date() + "\n");
+    }, 1000);
+    //response.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
