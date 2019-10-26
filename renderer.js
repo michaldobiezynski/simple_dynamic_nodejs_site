ed5ba96cdff973e379bf99bb687face533176cea
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-26 16:47:40 +0100

    merging keyholders in my view with values fetched from the api

--- renderer.js
+++ renderer.js
@@ -1,9 +1,22 @@
 
 const fs = require('fs');
 
+    function mergeValues(values, content) {
+
+        for(let key in values) {
+            content = content.replace(`{{${key}}}`, values[key]);
+        }
+
+        return content;
+
+    }
+
     function view(templateName, values, response) {
 
-    const fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    let fileContents = fs.readFileSync('./views/' + templateName + '.html',
+                            'utf8');
+
+        fileContents = mergeValues(values, fileContents);
 
             response.write(fileContents);
 
